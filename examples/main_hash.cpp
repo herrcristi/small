68dcfa0bc9a33673b5626511bfe051175db6af5d
Cristian Herghelegiu <cristian.herghelegiu@bullguard.com>
2019-10-25 16:19:36 +0300

    add examples for hash and util

---
+++ examples/main_hash.cpp
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <cstdio>
+#include <iostream>
+#include <condition_variable>
+#include <thread>
+#include <map>
+
+#include "small/include/hash.h"
+
+
+int main()
+{
+    std::cout << "hello" << std::endl;
+
+    unsigned long long hash = small::quick_hash( "some text", 9/*strlen*/, 0 );
+    
+    unsigned long long h1 = small::quick_hash( "some ", 5/*strlen*/, 0 );
+    unsigned long long h2 = small::quick_hash( "text",  4/*strlen*/, h1 );
+
+    
+    return 0;
+}
---
+++ examples/main_util.cpp
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <cstdio>
+#include <iostream>
+#include <condition_variable>
+#include <thread>
+#include <map>
+
+#include "small/include/util.h"
+
+
+int main()
+{
+    std::cout << "hello" << std::endl;
+
+    std::map<std::string, int, small::icasecmp> m;
+    m["B"] = 2;
+    m["a"] = 1;
+
+    int r = small::stricmp( "a", "C" );
+
+
+
+    return 0;
+}
