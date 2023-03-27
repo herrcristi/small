82cbbf2fc2c886ff9fd144c9bf7ec84a7d6aa471
Cristian Herghelegiu <herr.cristi@gmail.com>
2023-03-27 12:36:32 +0300

    Add .vscode to gitignore and clear warning for base64 example

--- examples/main_base64.cpp
+++ examples/main_base64.cpp
@@ -8,17 +8,15 @@
 // make sure the path is included correct
 #include "small/include/base64.h"
 
-
 int main()
 {
     std::cout << "hello" << std::endl;
 
-    std::string b64 = small::tobase64_s( "hello world" );
-    std::vector<char> vb64 = small::tobase64_v( "hello world", 11 );
-   
-    std::string decoded = small::frombase64_s( b64 );
-    std::vector<char> vd64 = small::frombase64_v( b64 );
+    std::string b64 = small::tobase64_s(std::string("hello world"));
+    std::vector<char> vb64 = small::tobase64_v("hello world", 11);
 
-   
+    std::string decoded = small::frombase64_s(b64);
+    std::vector<char> vd64 = small::frombase64_v(b64);
+
     return 0;
 }
